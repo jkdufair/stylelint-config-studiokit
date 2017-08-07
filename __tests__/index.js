ceb4b4bf4bd0b727a9eaecb6b8ba5c98f7a63f84
Will Grauvogel <builtbywill@gmail.com>
2017-08-07 11:31:13 -0400

    fix unit tests, add eslint-config-studiokit (needs updated later) for auto formatting of js code

--- __tests__/index.js
+++ __tests__/index.js
@@ -1,5 +1,3 @@
-'use strict'
-
 const config = require('../')
 const fs = require('fs')
 const stylelint = require('stylelint')
@@ -13,20 +11,16 @@ describe('flags no warnings with valid css', () => {
 	beforeEach(() => {
 		result = stylelint.lint({
 			code: validCss,
-			config,
+			config
 		})
 	})
 
 	it('did not error', () => {
-		return result.then(data => (
-			expect(data.errored).toBeFalsy()
-		))
+		return result.then(data => expect(data.errored).toBeFalsy())
 	})
 
 	it('flags no warnings', () => {
-		return result.then(data => (
-			expect(data.results[0].warnings.length).toBe(0)
-		))
+		return result.then(data => expect(data.results[0].warnings.length).toBe(0))
 	})
 })
 
@@ -36,49 +30,51 @@ describe('flags warnings with invalid css', () => {
 	beforeEach(() => {
 		result = stylelint.lint({
 			code: invalidCss,
-			config,
+			config
 		})
 	})
 
 	it('did error', () => {
-		return result.then(data => (
-			expect(data.errored).toBeTruthy()
-		))
+		return result.then(data => expect(data.errored).toBeTruthy())
 	})
 
-	it('flags one warning', () => {
-		return result.then(data => (
-			expect(data.results[0].warnings.length).toBe(1)
-		))
+	it('flags three warning', () => {
+		return result.then(data => expect(data.results[0].warnings.length).toBe(3))
 	})
 
-	it('correct warning text', () => {
-		return result.then(data => (
-			expect(data.results[0].warnings[0].text).toBe('Expected a leading zero (number-leading-zero)')
-		))
+	it('flags indentation', () => {
+		return result.then(data => {
+			expect(data.results[0].warnings[0]).toEqual({
+				line: 2,
+				column: 3,
+				rule: 'indentation',
+				severity: 'error',
+				text: 'Expected indentation of 1 tab (indentation)'
+			})
+		})
 	})
 
-	it('correct rule flagged', () => {
-		return result.then(data => (
-			expect(data.results[0].warnings[0].rule).toBe('number-leading-zero')
-		))
+	it('flags no-missing-end-of-source-newline', () => {
+		return result.then(data => {
+			expect(data.results[0].warnings[1]).toEqual({
+				line: 3,
+				column: 1,
+				rule: 'no-missing-end-of-source-newline',
+				severity: 'error',
+				text: 'Unexpected missing end-of-source newline (no-missing-end-of-source-newline)'
+			})
+		})
 	})
 
-	it('correct severity flagged', () => {
-		return result.then(data => (
-			expect(data.results[0].warnings[0].severity).toBe('error')
-		))
-	})
-
-	it('correct line number', () => {
-		return result.then(data => (
-			expect(data.results[0].warnings[0].line).toBe(2)
-		))
-	})
-
-	it('correct column number', () => {
-		return result.then(data => (
-			expect(data.results[0].warnings[0].column).toBe(8)
-		))
+	it('flags number-leading-zero', () => {
+		return result.then(data => {
+			expect(data.results[0].warnings[2]).toEqual({
+				line: 2,
+				column: 8,
+				rule: 'number-leading-zero',
+				severity: 'error',
+				text: 'Expected a leading zero (number-leading-zero)'
+			})
+		})
 	})
 })
